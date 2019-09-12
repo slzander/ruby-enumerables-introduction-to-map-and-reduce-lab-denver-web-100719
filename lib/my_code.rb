b802221abd283b6f1e394663cdf46fcdcdebd92d
Stacey Zander <staceyzander@gmail.com>
2019-09-12 00:41:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,8 +7,8 @@ def map_to_negativize (source_array)
   return source_array
 end
 
-def map_to_no_change(source_array)
-  return source_array
+def map_to_no_change(array)
+  return array
 end
 
 def map_to_double (source_array)
